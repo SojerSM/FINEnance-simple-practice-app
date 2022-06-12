85976da85e7f5b8d896816c21cab6eb63e453046
Sebastian Mazur <seb.maz1996@gmail.com>
2022-06-12 22:34:59 +0200

    Add files via upload
    Almost ready, added: sign in system, user data & summary updating, adding elements to the lists and sorting those in order to beeing an income or spending, visual representation inside list containers, calculating percentage of savings and comparing it to the remaining amount

--- script.js
+++ script.js
@@ -4,21 +4,42 @@
 // SELECTING ELEMENTS */
 ////////////////////////
 const labelRandLogin = document.getElementById('generated-login-label');
+const labelUserWelcome = document.getElementById('welcome-user');
+const labelDateMessage = document.getElementById('date-message');
+const labelTotalIncome = document.getElementById('total-incomes');
+const labelTotalSpending = document.getElementById('total-spendings');
+const labelSummary = document.getElementById('summary-amount');
 
 const btnNewAccount = document.querySelector('.btn-create-new-account');
 const btnMainPage = document.querySelector('.btn-back-to-main-page');
 const btnSignUp = document.querySelector('.btn-sign-up-submit');
+const btnLogin = document.querySelector('.btn-login-submit');
 const btnModal = document.querySelector('.btn-sign-in');
+const btnLogout = document.querySelector('.btn-logout');
+const btnAdd = document.querySelector('.btn-income-spending');
+const btnSort = document.querySelector('.btn-sort-lists');
+const btnClearAll = document.querySelector('.btn-clear-all');
+const btnSavingsSubmit = document.querySelector('.btn-savings-submit');
 
 const containerLogin = document.querySelector('.login-container');
 const containerSignUp = document.querySelector('.sign-up-container');
-const modalWrapper = document.querySelector('.sign-up-modal-wrapper');
+const containerApp = document.querySelector('.app-container');
+const wrapperModal = document.querySelector('.sign-up-modal-wrapper');
 const overlay = document.querySelector('.overlay');
+const wrapperWelcome = document.querySelector('.welcome-wrapper');
+const containerIncomes = document.querySelector('.incomes-container');
+const containerSpendings = document.querySelector('.spendings-container');
 
 const inputSignUpName = document.querySelector('.sign-up-name');
 const inputSignUpYear = document.querySelector('.sign-up-birth-year');
 const inputSignUpPass = document.querySelector('.sign-up-password');
 const inputSignUpPassRep = document.querySelector('.sign-up-password-rep');
+const inputLoginUser = document.querySelector('.login-user');
+const inputLoginPass = document.querySelector('.login-password');
+const inputDesignation = document.querySelector('.income-spending-bar');
+const selectTypesList = document.querySelector('.income-spending-types-list');
+const inputAmount = document.querySelector('.amount-bar');
+const inputSavings = document.querySelector('.input-savings');
 
 const warnSignUpName = document.querySelector('.sign-up-name-warning');
 const warnSignUpYear = document.querySelector('.sign-up-birth-year-warning');
@@ -27,11 +48,28 @@ const warnSignUpPassRep = document.querySelector(
   '.sign-up-password-rep-warning'
 );
 const warnSignUpSubmit = document.querySelector('.sign-up-submit-warning');
+const warnLoginUser = document.querySelector('.login-username-warning');
+const warnLoginPass = document.querySelector('.login-password-warning');
+const warnLoginSubmit = document.querySelector('.login-submit-warning');
+const warnAddBar = document.querySelector('.income-spending-warning');
+const warnSavings = document.querySelector('.savings-bar-warning');
 
 //////////////
 /* INITIALS */
 //////////////
-const accounts = [];
+const accounts = [
+  {
+    firstName: 'sebastian',
+    username: 'seb96dx',
+    birthYear: 1996,
+    password: 'margonem',
+    incomes: [],
+    spendings: [],
+    totalIncome: 0,
+    totalSpending: 0,
+  },
+];
+let currentAccount, summary;
 
 ///////////////
 /* FUNCTIONS */
@@ -41,11 +79,70 @@ const toggleSignWindows = function () {
   containerSignUp.classList.toggle('hidden');
 };
 
+const toggleAppWindow = function () {
+  containerLogin.classList.toggle('hidden');
+  containerApp.classList.toggle('hidden');
+  wrapperWelcome.classList.toggle('hidden');
+  btnLogout.classList.toggle('hidden');
+};
+
 const clearSignUpInputs = function () {
   inputSignUpName.value = '';
   inputSignUpYear.value = '';
   inputSignUpPass.value = '';
   inputSignUpPassRep.value = '';
+};
+
+const clearLoginInputs = function () {
+  inputLoginUser.value = '';
+  inputLoginPass.value = '';
+};
+
+const clearAddBarInputs = function () {
+  inputDesignation.value = '';
+  selectTypesList.value = '';
+  inputDesignation.value = '';
+};
+
+const clearSignUpWarnings = function () {
+  warnSignUpName.classList.add('hidden');
+  warnSignUpPass.classList.add('hidden');
+  warnSignUpPassRep.classList.add('hidden');
+  warnSignUpYear.classList.add('hidden');
+};
+
+const clearLoginWarnings = function () {
+  warnLoginUser.classList.add('hidden');
+  warnLoginPass.classList.add('hidden');
+};
+
+const displaySignUpModal = function (username) {
+  overlay.classList.remove('hidden');
+  wrapperModal.classList.remove('hidden');
+  labelRandLogin.textContent = username;
+};
+
+const updateUserData = function (account) {
+  let [first, ...others] = account.firstName;
+  labelUserWelcome.textContent = first.toUpperCase().concat(...others);
+  labelDateMessage.textContent = getFormatedDate();
+  updateSummary(currentAccount);
+};
+
+const updateSummary = function (account) {
+  labelTotalIncome.textContent = account.totalIncome + '$';
+  labelTotalSpending.textContent = account.totalSpending + '$';
+  labelSummary.textContent = account.totalIncome - account.totalSpending + '$';
+};
+
+const getFormatedDate = function () {
+  const currentDate = new Date();
+  const hours = `${currentDate.getHours()}`.padStart(2, '0');
+  const minutes = `${currentDate.getMinutes()}`.padStart(2, '0');
+  const dateStr = `${currentDate.toLocaleDateString('en-GB', {
+    weekday: 'long',
+  })}, ${hours}:${minutes}`;
+  return dateStr;
 };
 
 const checkSignUpName = function (name) {
@@ -64,7 +161,7 @@ const checkSignUpName = function (name) {
 const checkSignUpYear = function (year) {
   const currentYear = new Date().getFullYear();
   const inputYear = +inputSignUpYear.value;
-  if (inputYear >= 1900 && inputYear < currentYear - 1) {
+  if (inputYear >= 1900 && inputYear < currentYear) {
     year = inputYear;
     warnSignUpYear.classList.add('hidden');
   } else {
@@ -86,7 +183,7 @@ const checkSignUpPassword = function (pass) {
   return pass;
 };
 
-const comparePasswords = function (pass, rep) {
+const compareSignUpPasswords = function (pass, rep) {
   if (pass === rep) {
     warnSignUpPassRep.classList.add('hidden');
     return true;
@@ -107,10 +204,61 @@ const createUsername = function (name, year) {
   return username;
 };
 
-const displaySignUpModal = function (username) {
-  overlay.classList.remove('hidden');
-  modalWrapper.classList.remove('hidden');
-  labelRandLogin.textContent = username;
+const checkLoginData = function (id) {
+  clearLoginWarnings();
+  if (!(id >= 0)) {
+    warnLoginUser.classList.remove('hidden');
+    warnLoginUser.textContent = `User doesn't exist`;
+    clearLoginInputs();
+  } else {
+    currentAccount = accounts[id];
+    console.log(currentAccount);
+    if (currentAccount.password === inputLoginPass.value) {
+      toggleAppWindow();
+      updateUserData(currentAccount);
+    } else {
+      warnLoginPass.classList.remove('hidden');
+      warnLoginPass.textContent = 'Incorrect password';
+    }
+    clearLoginInputs();
+  }
+};
+
+const updateIncomes = function () {
+  containerIncomes.innerHTML = '<p id="incomes-title">Incomes</p>';
+  currentAccount.incomes.forEach((income, id) => {
+    const htmlEl = `<div class="income-wrapper">
+    <div class="left-column">
+      <p class="income-name">${income.designation}</p>
+    </div>
+    <div class="right-column">
+      <p class="income-type">${income.type}</p>
+      <p class="income-amount">${income.amount}$</p>
+    </div>
+  </div>`;
+    containerIncomes.insertAdjacentHTML('beforeend', htmlEl);
+  });
+};
+
+const updateSpendings = function () {
+  containerSpendings.innerHTML = '<p id="spendings-title">Spendings</p>';
+  currentAccount.spendings.forEach((spending, id) => {
+    const htmlEl = `<div class="spending-wrapper">
+    <div class="left-column">
+      <p class="spending-name">${spending.designation}</p>
+    </div>
+    <div class="right-column">
+      <p class="spending-type">${spending.type}</p>
+      <p class="spending-amount">${spending.amount}$</p>
+    </div>
+  </div>`;
+    containerSpendings.insertAdjacentHTML('beforeend', htmlEl);
+  });
+};
+
+const compareSummaryAmount = function (amount) {
+  summary = currentAccount.totalIncome - currentAccount.totalSpending;
+  return amount > summary ? false : true;
 };
 
 ////////////////////
@@ -124,7 +272,7 @@ btnMainPage.addEventListener('click', () => {
   toggleSignWindows();
 });
 
-// Sign up
+// SIGN UP
 btnSignUp.addEventListener('click', e => {
   e.preventDefault();
   let name, year, pass, passRep;
@@ -138,13 +286,13 @@ btnSignUp.addEventListener('click', e => {
     warnSignUpSubmit.classList.remove('hidden');
     warnSignUpSubmit.textContent = 'Complete the form';
     clearSignUpInputs();
-    console.log(document.querySelector('.sign-up-password').value);
+    clearSignUpWarnings();
   } else {
     warnSignUpSubmit.classList.add('hidden');
     name = checkSignUpName(name);
     year = checkSignUpYear(year);
     pass = checkSignUpPassword(pass);
-    passRep = comparePasswords(pass, inputSignUpPassRep.value);
+    passRep = compareSignUpPasswords(pass, inputSignUpPassRep.value);
     clearSignUpInputs();
     // Push new object to the array if all data are correct
     if (name && year && pass && passRep) {
@@ -154,6 +302,10 @@ btnSignUp.addEventListener('click', e => {
         username: username,
         birthYear: year,
         password: pass,
+        incomes: [],
+        spendings: [],
+        totalIncome: 0,
+        totalSpending: 0,
       });
       displaySignUpModal(username);
     }
@@ -164,7 +316,125 @@ btnSignUp.addEventListener('click', e => {
 
 btnModal.addEventListener('click', () => {
   overlay.classList.add('hidden');
-  modalWrapper.classList.add('hidden');
+  wrapperModal.classList.add('hidden');
   containerSignUp.classList.add('hidden');
   containerLogin.classList.remove('hidden');
+  warnLoginSubmit.classList.add('hidden');
+  clearLoginInputs();
+  clearLoginWarnings();
+});
+
+// LOGIN
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+  // check if accounts array contains element with passed username
+  let userId = accounts.findIndex(account => {
+    return account.username === inputLoginUser.value;
+  });
+  // check if any field is empty
+  if (!inputLoginUser.value || !inputLoginPass.value) {
+    warnLoginSubmit.classList.remove('hidden');
+    warnLoginSubmit.textContent = 'Complete the form';
+    clearLoginInputs();
+    clearLoginWarnings();
+  } else {
+    warnLoginSubmit.classList.add('hidden');
+    checkLoginData(userId);
+  }
+});
+
+btnLogout.addEventListener('click', () => {
+  toggleAppWindow();
+});
+
+// Adding a new element to the arrays of incomes/spendings
+btnAdd.addEventListener('click', e => {
+  e.preventDefault();
+  const type = selectTypesList.value;
+  // check if any field is empty
+  if (!inputDesignation.value || !type || !inputAmount.value) {
+    clearAddBarInputs();
+    warnAddBar.classList.remove('hidden');
+    warnAddBar.textContent = 'Complete the form';
+  } else {
+    if (isNaN(+inputAmount.value)) {
+      console.log('Is not a number');
+      warnAddBar.classList.remove('hidden');
+      warnAddBar.textContent = 'Amount must be a number';
+    } else {
+      warnAddBar.classList.add('hidden');
+      if (type === 'Salary' || type === 'Interest') {
+        currentAccount.incomes.push({
+          designation: inputDesignation.value.trim(),
+          type: type,
+          amount: inputAmount.value,
+        });
+        currentAccount.totalIncome += +inputAmount.value;
+        updateIncomes();
+      }
+      if (type === 'Fixed' || type === 'Irregular') {
+        currentAccount.spendings.push({
+          designation: inputDesignation.value.trim(),
+          type: type,
+          amount: inputAmount.value,
+        });
+        currentAccount.totalSpending += +inputAmount.value;
+        updateSpendings();
+      }
+    }
+  }
+  updateSummary(currentAccount);
+  clearAddBarInputs();
+});
+
+btnSort.addEventListener('click', e => {
+  e.preventDefault();
+  currentAccount.incomes.sort((a, b) => {
+    return b.amount - a.amount;
+  });
+  currentAccount.spendings.sort((a, b) => {
+    return b.amount - a.amount;
+  });
+  updateIncomes();
+  updateSpendings();
+  clearAddBarInputs();
+});
+
+btnClearAll.addEventListener('click', e => {
+  e.preventDefault();
+  currentAccount.incomes = [];
+  currentAccount.spendings = [];
+  updateIncomes();
+  updateSpendings();
+  clearAddBarInputs();
+  updateSummary(currentAccount);
+});
+
+btnSavingsSubmit.addEventListener('click', e => {
+  e.preventDefault();
+  if (!inputSavings.value) {
+    warnSavings.classList.remove('hidden');
+    warnSavings.textContent = 'Complete the form';
+    inputSavings.value = '';
+  } else {
+    if (
+      Number.isInteger(+inputSavings.value) &&
+      +inputSavings.value >= 1 &&
+      +inputSavings.value < 100
+    ) {
+      warnSavings.classList.add('hidden');
+      let amount = currentAccount.totalIncome * (inputSavings.value / 100);
+      console.log(amount);
+      if (compareSummaryAmount(amount)) {
+        console.log('Amount lower than actual savings');
+      } else {
+        console.log('Amount higher than actual savings');
+      }
+      console.log('Proper number');
+    } else {
+      warnSavings.classList.remove('hidden');
+      warnSavings.textContent = 'Must be an integer in range of 1-100';
+      inputSavings.value = '';
+    }
+  }
 });
